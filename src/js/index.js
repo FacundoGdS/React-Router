0ea443316583dea0eeca99030430648ee5a28bae
Facundo <facundogds@gmail.com>
2022-11-16 00:33:32 +0000

    First view

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,9 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Layout from '././component/VIEWS/Layout.jsx';
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Layout />, document.querySelector("#app"));
+
+//Lo deje por el minuto 20 o 29 vos fijate
